55df51e3d3e4163a0b0475340660b2224deef57d
Lorekgaston <lorekgaston@gmail.com>
2021-03-30 18:19:35 -0300

    finished homePage

--- src/components/Header/HeaderMenu/HeaderMenu.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-const HeaderMenu = () => {
-  return <div></div>;
-};
-
-export default HeaderMenu;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,107 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { userAction } from '../../redux/actions/user';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import { Avatar } from '@material-ui/core';
-
 import './Home.scss';
-
-const colors = [
-  '#16c591',
-  '#277caa',
-  '#21c1d6',
-  '#f7b957',
-  '#d2293d',
-  '#6c76cc',
-  '#983873',
-  '#7460ee',
-  '#b50003',
-  '#761718',
-  '#040301',
-  '#65b954'
-];
 
 const Home = () => {
   const { user } = useSelector(state => state.login);
-  const { userList } = useSelector(state => state.users);
-  const [isOpen, setIsOpen] = useState(false);
-  const dispatch = useDispatch();
-  const { users } = userList || {};
-  console.log(users);
+  const history = useHistory();
 
-  const handleRandomColor = () =>
-    colors[Math.floor(Math.random() * colors.length)];
-
-  const handelMenuOpen = userIdx =>
-    isOpen === userIdx ? setIsOpen(null) : setIsOpen(userIdx);
-
-  useEffect(() => {
-    dispatch(userAction.getAllUsers());
-  }, []);
-
-  const renderUsers = () => {
-    return users?.map((user, userIdx) => {
-      const { userName, role } = user;
-      return (
-        <li key={userName + userIdx}>
-          <Avatar>{userName?.charAt(0).toUpperCase()}</Avatar>
-          <div className="Username">
-            <h6>{userName}</h6>
-            <span>Role: {role}</span>
-          </div>
-          <div className="Icon">
-            <div
-              className="Icon__container"
-              onClick={() => handelMenuOpen(userIdx)}
-            >
-              <MoreVertIcon />
-            </div>
-            {isOpen === userIdx ? (
-              <div className="Menu">
-                <ul menu="Menu__list">
-                  <li className="Menu__listItem">
-                    <span>
-                      <p>Role</p>
-                      <ArrowRightIcon />
-                      <div className="RolesMenu">
-                        <ul className="RolesMenu__rolesList">
-                          <li className="RoleMenu__roles">
-                            <span>Admin</span>
-                          </li>
-                          <li className="RoleMenu__roles">
-                            <span>Moderator</span>
-                          </li>
-                          <li className="RoleMenu__roles">
-                            <span>user</span>
-                          </li>
-                        </ul>
-                      </div>
-                    </span>
-                  </li>
-                  <li className="Menu__listItem">
-                    <span>Delete</span>
-                  </li>
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </li>
-      );
-    });
-  };
   return (
     <div className="Home">
-      <div className="Home__userInfo">
-        <h1>Welcom {user.user.userName}</h1>
-        <h2>Email: {user.user.email}</h2>
-        <h2>Role: {user.user.role}</h2>
-        <h2>ID: {user.user._id}</h2>
-      </div>
-      <div className="Home__userList">
-        <div className="Home__userList_list">
-          <ul>{renderUsers()}</ul>
+      <div className="Home__Header"></div>
+      <div className="Home__Bottom">
+        <div className="Home__Bottom_user">
+          <div className="Avatar">
+            <div className="Avatar__container">
+              <div className="Avatar__container_image">
+                <Avatar />
+              </div>
+            </div>
+          </div>
+          <div className="Info">
+            <div className="Info__name">
+              <h1>Welcom {user.user.userName}!</h1>
+            </div>
+            <div className="Info__rest">
+              <h6>{user.user.role}</h6>
+              <h6>{user.user.email}</h6>
+              <h6>{user.user._id}</h6>
+            </div>
+          </div>
+
+          <button
+            className="LogOutButton"
+            onClick={() => history.push('/login')}
+          >
+            Log Out
+          </button>
         </div>
       </div>
     </div>
